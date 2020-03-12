d18dbef9bd3569b0cfeffb23466ea083c2834c87
Bugra Aydogar <bugraaydogar@gmail.com>
2020-03-12 20:56:43 +0300

    Customuser model is created for features and rest_framework is added to project

--- src/atom/settings.py
+++ src/atom/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Django Rest Framework
+    'rest_framework',
+    
+    # Custom Applications
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = "users.CustomUser"
--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+# [BAY] This is a preparation for the future
+# At the moment, we can use the example user from Django
+# However, in the future, we may need to adjust it.
+# Please look at AbstractUser Class for more info
+class CustomUser(AbstractUser):
+    pass
