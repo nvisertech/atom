d466ef2ceeeda53f0bf3f9812c8b192ef052a01f
Bugra Aydogar <bugraaydogar@gmail.com>
2020-03-15 17:56:14 +0300

    Vuetify is integrated

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,10 +1,14 @@
 import Vue from "vue";
+import vuetify from '@/plugins/vuetify' // path to vuetify export
 import App from "./App.vue";
 import router from "./router";
+import store from "./store";
 
 Vue.config.productionTip = false;
 
 new Vue({
+  vuetify,
   router,
+  store,
   render: h => h(App)
 }).$mount("#app");
